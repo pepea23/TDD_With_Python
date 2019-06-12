71255d5462a525ae29f419668382712ce34c7874
Ittidate <pepea.23@live.com>
2019-06-12 15:07:13 +0700

    :white_check_mark: change expected

--- TDD.py
+++ TDD.py
@@ -5,24 +5,24 @@ lifts = [2, 3, 3, 5]
 
 class LiftTest(unittest.TestCase):
     def test_curent_floor_1_should_be_lift1(self):
-        expexted = 1
+        expexted = 2
         result = handleLift(1)
         self.assertEqual(result, expexted)
     
     def test_curent_floor_2_should_be_lift1(self):
-        expexted = 1
+        expexted = 2
         result = handleLift(2)
         self.assertEqual(result, expexted)
 
     def test_curent_floor_3_should_be_lift2(self):
-        expexted = 2
+        expexted = 3
         result = handleLift(3)
         self.assertEqual(result, expexted)
 
 
 def handleLift(num):
-        lift =  min(range(len(lifts)), key=lambda i: abs(lifts[i]-num))
-        return lift + 1
+        id =  min(range(len(lifts)), key=lambda i: abs(lifts[i]-num))
+        return lifts[id]
 
 
 unittest.main()
