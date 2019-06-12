94819c2e128222c021b4af5ede5bbeaed843f7b9
Ittidate <pepea.23@live.com>
2019-06-12 15:28:01 +0700

    เปลี่ยนเป็น dict แล้วก้แก้การรับค่าของ func เป็น 2 param

--- TDD.py
+++ TDD.py
@@ -2,27 +2,29 @@ import unittest
 
 # [lift1 ,lift2, lift3, lift4]
 lifts = [2, 3, 3, 5]
+lifts = [{'floor':2,'status':'up'}, {'floor':3,'status':'down'}, {'floor':3,'status':'none'}, {'floor':5,'status':'down'}]
+
 
 class LiftTest(unittest.TestCase):
     def test_curent_floor_1_should_be_lift1(self):
         expexted = 2
-        result = handleLift(1)
+        result = handleLift(1,'up')
         self.assertEqual(result, expexted)
     
     def test_curent_floor_2_should_be_lift1(self):
         expexted = 2
-        result = handleLift(2)
+        result = handleLift(2,'up')
         self.assertEqual(result, expexted)
 
     def test_curent_floor_3_should_be_lift2(self):
         expexted = 3
-        result = handleLift(3)
+        result = handleLift(3,'up')
         self.assertEqual(result, expexted)
 
 
-def handleLift(num):
-        id =  min(range(len(lifts)), key=lambda i: abs(lifts[i]-num))
-        return lifts[id]
+def handleLift(num,btn):
+        id =  min(range(len(lifts)), key=lambda i: abs(lifts[i].get('floor')-num))
+        return lifts[id].get('floor')
 
 
 unittest.main()
