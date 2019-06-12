b1c5596c2c7e9cfff7c14d08c7573c9f7123f0d3
Ittidate <pepea.23@live.com>
2019-06-12 14:56:53 +0700

    :recycle: refactor

--- TDD.py
+++ TDD.py
@@ -1,23 +1,24 @@
 import unittest
 
-# [lift1 ,lift2 , lift3]
-lifts = [2,3,4]
+# [lift1 ,lift2, lift3]
+lifts = [2, 3, 4]
 
 class LiftTest(unittest.TestCase):
     def test_curent_floor_1_should_be_lift1(self):
         expexted = 1
         result = handleLift(1)
-        self.assertEqual(result,expexted)
+        self.assertEqual(result, expexted)
     
     def test_curent_floor_2_should_be_lift1(self):
         expexted = 1
         result = handleLift(2)
-        self.assertEqual(result,expexted)
+        self.assertEqual(result, expexted)
 
     def test_curent_floor_3_should_be_lift2(self):
         expexted = 2
         result = handleLift(3)
-        self.assertEqual(result,expexted)
+        self.assertEqual(result, expexted)
+
 
 def handleLift(num):
         lift =  min(range(len(lifts)), key=lambda i: abs(lifts[i]-num))
