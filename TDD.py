62780ab4e6474cb89b3fa4e78a8606d8299683fb
Ittidate <pepea.23@live.com>
2019-06-12 14:17:02 +0700

    :white_check_mark: update test01

--- TDD.py
+++ TDD.py
@@ -1,14 +1,12 @@
 import unittest
 
-lifts = [{1:2},{2:3},{3:4}]
+lifts = [{'lift1':2},{'lift2':3},{'lift3':4}]
 
 class LiftTest(unittest.TestCase):
     def test_curent_floor_1_should_be_lift1(self):
-        expexted = 1
+        expexted = 'lift1'
         result = handleLift(1)
         self.assertEqual(result,expexted)
     
-
-
 
 unittest.main()
