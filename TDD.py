00e456e660aa9e4f08b358f793169b90ad414bd8
Ittidate <pepea.23@live.com>
2019-06-13 12:22:24 +0700

    :white_check_mark: แก้เรื่องการเพื่มเงื่อนไขถ้าลิฟอยู่ชั้น 2 กำลังขึ้นจะไม่ลงมารับชั้นที่ต่ำกว่าหรือ ถ้า ลิฟอยู่ ชั้น 3 กำลังลงจะไม่ไปรับชั้นที่สูงกว่า

--- TDD.py
+++ TDD.py
@@ -1,12 +1,12 @@
 import unittest
 
 # [lift1 ,lift2, lift3, lift4]
-lifts = [{'liftID': 1, 'floor': 2, 'status': 'up'}, {'liftID': 2, 'floor': 3, 'status': 'down'}, {'liftID': 3, 'floor': 3, 'status': 'up'}, {'liftID': 4, 'floor': 3, 'status': 'down'}]
+lifts = [{'liftID': 1, 'floor': 2, 'status': 'up'}, {'liftID': 2, 'floor': 3, 'status': 'down'}, {'liftID': 3, 'floor': 3, 'status': 'none'}, {'liftID': 4, 'floor': 3, 'status': 'down'}]
 
 
 class LiftTest(unittest.TestCase):
     def test_curent_floor_1_should_be_lift1(self):
-        expexted = [1,'up']
+        expexted = [3,'none']
         result = handleLift(1,'up')
         self.assertEqual(result, expexted)
     
@@ -16,7 +16,7 @@ class LiftTest(unittest.TestCase):
         self.assertEqual(result, expexted)
 
     def test_curent_floor_3_but_have_3_lift_same_floorshould_be_lift2(self):
-        expexted = [3,'up']
+        expexted = [3,'none']
         result = handleLift(3,'up')
         self.assertEqual(result, expexted)
 
@@ -25,7 +25,7 @@ def handleLift(num,btn):
         temps = []
         size = len(lifts)
         for i in range(size):
-                if lifts[i].get('status') == btn:
+                if lifts[i].get('status') == btn or lifts[i].get('status') == 'none':
                         temps.insert(i,lifts[i])
         id =  min(range(len(temps)), key=lambda i: abs(temps[i].get('floor')-num))
         return [temps[id].get('liftID'),temps[id].get('status')]
